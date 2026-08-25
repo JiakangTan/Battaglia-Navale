6ddd6390f979eab012d9f313104ab5f2c533d45e
Tan <j.tan.4171@vallauri.edu>
2026-08-25 16:19:49 +0200

    Cronometro e placeholder funzioni

--- js/index.js
+++ js/index.js
@@ -9,11 +9,18 @@ let naviRimastePlayer = 0;
 let naviRimasteNemico = 0;
 let gameOver = false;
 
+// cronometro
+let interval = null;
+let secondi = 0;
+let minuti = 0;
+let Attivo = false;
+
 const giocatoreGriglia = document.getElementById('griglia-giocatore');
 const nemicoGriglia = document.getElementById('griglia-nemico'); 
 const statoPartita = document.getElementById('stato');
 const btnRicomincia = document.getElementById('btn-ricomincia');
 
+const cronometro = document.getElementById('cronom');
 
 function inizializza_gioco(){
     gameOver = false;
@@ -27,10 +34,39 @@ function inizializza_gioco(){
     genera_Griglia(giocatoreGriglia,"giocatore")
     genera_Griglia(nemicoGriglia,"nemico")
 
+    naviRimastePlayer = 17;
+    naviRimasteNemico = 17; // celle contenenti parti di navi
 
+    
+    
+    
 }
 
+function IniziaCronometro(){
+    if(Attivo == true)
+        clearInterval(interval);
 
+    interval = setInterval(() => {
+        if(secondi + 1 == 60)
+        {
+            secondi = 0;
+            minuti++
+        }else
+            secondi++;
+
+        if(minuti <10)
+            if(secondi <10)
+                cronometro.innerHTML = `0${minuti}:0${secondi}`
+            else
+                cronometro.innerHTML = `0${minuti}:${secondi}`
+        else
+            if(secondi <10)
+                cronometro.innerHTML = `${minuti}:0${secondi}`
+            else
+                cronometro.innerHTML = `${minuti}:${secondi}`
+    }, 1000);
+    Attivo = true;
+}
 
 function genera_Griglia(griglia,tipo){
     for(let r = 0;r < grandezza_Griglia;r++){
@@ -47,6 +83,30 @@ function genera_Griglia(griglia,tipo){
             griglia.appendChild(cella);
         }
     }
+}
+
+function posiziona_navi(){
+
+}
+
+function controlloPosizionamento(){
+
+}
+
+function posizionamento(){
+
+}
+
+function ottieniCella(){
+
+}
+
+function clickCellaNemica(){
+
+}
+
+function turnoAI(){
+
 }
 
 inizializza_gioco();
